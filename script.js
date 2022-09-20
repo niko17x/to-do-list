efb60025bc9c249a07430f1202742f826dd6143b
Niko Shin <nshin17@gmail.com>
2022-09-19 19:36:02 -0700

    Updated html file and cleaned up js file by updating the object creation through an event listener instead of using a function

--- script.js
+++ script.js
@@ -1,37 +1,25 @@
 // Dealing with local storage:
-const LOCAL_STORAGE_LIST_KEY = 'task.projectItemList';
-// const LOCAL_STORAGE_LIST_ID_KEY = 'task.selectedListId';
 
-let projectItemList = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || []; // Get lists data from local storage || if none exists, then create new empty array.
-// let selectedListId = localStorage.getItem(LOCAL_STORAGE_LIST_ID_KEY);
+
+// Get lists data from local storage || if none exists, then create new empty array.
 
 const projectsContainer = document.querySelector('.projects-container');
 const createProjectInput = document.querySelector('.create-project-input');
-const createProjectBtn = document.querySelector('.create-project-btn');
 const projectItem = document.querySelector('.project-item');
 const navbar = document.querySelector('.navbar');
 const listTitle = document.querySelector('.list-title');
+const closeBtn = document.querySelector('.close-btn');
+const createProjectForm = document.querySelector('.create-project-form');
 
-// Local storage:
-function save() {
-    console.log(projectItemList.length)
-    localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(projectItemList));
-    // localStorage.setItem(LOCAL_STORAGE_LIST_ID_KEY, selectedListId);
-};
 
-function saveRender() {
-    save();
-    render();
-};
 
 // Project items array:
-// const projectItems = [];
+const projectItemList = [];
 
 function render() {
     // Removes existing nodes so project items aren't duplicated for every loop being run:
     clearElement(projectsContainer);
     renderProjectItem();
-    saveRender();
 };
 
 // Dynamically creating new projects:
@@ -42,6 +30,7 @@ function renderProjectItem() {
         // divProjectItem.id = item.id;
         const spanCloseBtn = document.createElement('span');
         spanCloseBtn.classList.add('close-btn');
+        spanCloseBtn.id = item.id;
         spanCloseBtn.innerHTML = '&#10005';
         const liProjectItemTitle = document.createElement('li');
         liProjectItemTitle.classList.add('project-item-title');
@@ -63,41 +52,42 @@ function generateUniqueId(title) {
     return {
         id: Date.now().toString(), // Generate unique id.
         projectName: title,
+        tasks: [], // Unique todo tasks for each object in the array (todo list).
     }
-};
-
-// Validate the project name input value and push valid value into 'projectItems' array:
-function validateProjectInput() {
-    const getValue = createProjectInput.value;
-    // If str is null or empty, don't return any data:
-    if (getValue == null || getValue === '') return;
-    const newProjectTitle = generateUniqueId(getValue);
-    createProjectInput.value = null; // Reset the value once accepted.
-    projectItems.push(newProjectTitle); // Push valid new object to array.
 };
 
 function clearElement(element) {
     while (element.firstChild) {
         element.removeChild(element.firstChild);
     }
-}
+};
+
 
 
 
 //* EVENT LISTENERS:
-createProjectBtn.addEventListener('click', e => {
+createProjectForm.addEventListener('submit', e => {
     e.preventDefault();
-    validateProjectInput();
-    saveRender();
+    const getValue = createProjectInput.value;
+    // If str is null or empty, don't return any data:
+    if (getValue == null || getValue === '') return;
+    const newProjectTitle = generateUniqueId(getValue);
+    createProjectInput.value = null; // Reset the value once accepted.
+    projectItemList.push(newProjectTitle); // Push valid new object to array.
+    render();
 })
 
-// navbar.addEventListener('click', e => {
-//     if (e.target.tagName.toLowerCase() === 'li') {
-//         selectedListId = e.target.id
-//         listTitle.innerText = e.target.innerText;
-//         saveRender();
-//     }
+navbar.addEventListener('click', e => {
+    if (e.target.tagName.toLowerCase() === 'li') {
+        selectedListId = e.target.id
+        listTitle.innerText = e.target.innerText;
+    }
+});
+
+
+
+// document.querySelector('.delete-btn').addEventListener('click', e => {
+//     projectItemList = projectItemList.filter(item => item.id !== selectedListId) // Creates new list with filter.
+//     selectedListId = null;
+//     saveRender();
 // });
-
-
-// TODO: FIGURE OUT WHY THE LOCAL STORAGE ISN'T WORKING AS IT SHOULD.
